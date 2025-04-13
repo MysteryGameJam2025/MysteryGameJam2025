5aa637e86dcd6419b5d23fe770a153b43c631f51
Jack Brewer <j.brewer@hotmail.co.uk>
2025-04-13 18:00:59 +0100

    Started work on a decoding system

---
+++ Assets/Scripts/MessageDecoder/MessageDecoderController.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class MessageDecoderController : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text textField;
+    private TMP_Text TextField => textField;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
