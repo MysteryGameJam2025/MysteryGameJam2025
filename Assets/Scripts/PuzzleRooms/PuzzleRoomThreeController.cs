1729159700776aafd61f3eca477385fe445342f0
Pepperized <bradstap@hotmail.co.uk>
2025-04-17 21:36:36 +0100

    bunch of shader stuff

---
+++ Assets/Scripts/PuzzleRooms/PuzzleRoomThreeController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PuzzleRoomThreeController : MonoBehaviour
+{
+    [SerializeField]
+    private Symbol ruin;
+    private Symbol Ruin => ruin;
+    [SerializeField]
+    private Symbol fulfill;
+    private Symbol Fulfill => fulfill;
+    [SerializeField]
+    private Symbol entrust;
+    private Symbol Entrust => entrust;
+
+}
