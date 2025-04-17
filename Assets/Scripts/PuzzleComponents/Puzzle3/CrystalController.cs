814aba83c4cadc31ee6d1ce4d5c68403be125c30
Pepperized <bradstap@hotmail.co.uk>
2025-04-17 22:19:23 +0100

    Crystal drop animation

---
+++ Assets/Scripts/PuzzleComponents/Puzzle3/CrystalController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CrystalController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pickup;
+    private GameObject Pickup => pickup;
+    [SerializeField]
+    private AnimationClip fallClip;
+    private AnimationClip FallClip => fallClip;
+
+
+    private Animator Animator { get; set; }
+
+    public void DropCrystal()
+    {
+        Animator = GetComponent<Animator>();
+        Animator.CrossFade(FallClip.name, 0);
+    }
+}
--- Assets/Scripts/PuzzleRooms/PuzzleRoomThreeController.cs
+++ Assets/Scripts/PuzzleRooms/PuzzleRoomThreeController.cs
@@ -11,5 +11,47 @@ public class PuzzleRoomThreeController : MonoBehaviour
     [SerializeField]
     private Symbol entrust;
     private Symbol Entrust => entrust;
+    [SerializeField]
+    private StairsController stairs;
+    private StairsController Stairs => stairs;
+    [SerializeField]
+    private CrystalController crystal;
+    private CrystalController Crystal => crystal;
+
+    private bool IsStairsDestroyed { get; set; }
+    private bool IsBrokenEmitterFixed { get; set; }
+    private bool IsCrystalDropped { get; set; }
+
+    public void OnStairsSymbolPlateUsed(Symbol symbol)
+    {
+        if (!IsStairsDestroyed && symbol == Ruin)
+        {
+            Stairs.DestroyRealStairs();
+            IsStairsDestroyed = true;
+        }
+
+        if (IsStairsDestroyed && symbol == Fulfill)
+        {
+            Stairs.SetHologramStarisEnabled(true);
+        }
+        else
+        {
+            Stairs.SetHologramStarisEnabled(false);
+        }
+    }
+
+    public void OnEmitterSymbolPlateUsed(Symbol symbol)
+    {
+        if (!IsCrystalDropped && symbol == Ruin)
+        {
+            IsCrystalDropped = true;
+            Crystal.DropCrystal();
+        }
+    }
+
+    public void OnEmitterFixed()
+    {
+        IsBrokenEmitterFixed = true;
+    }
 
 }
