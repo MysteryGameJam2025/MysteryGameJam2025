f5e05d6e7e14a1872110042f4a6832fe4d516d68
Pepperized <bradstap@hotmail.co.uk>
2025-04-21 16:10:16 +0100

    gauntlet ui update

--- Assets/Scripts/Gauntlet/GauntletUIController.cs
+++ Assets/Scripts/Gauntlet/GauntletUIController.cs
@@ -18,6 +18,12 @@ public class GauntletUIController : MonoBehaviour
     [SerializeField]
     private RectTransform pivot;
     private RectTransform Pivot => pivot;
+    [SerializeField]
+    private RectTransform leftArrow;
+    private RectTransform LeftArrow => leftArrow;
+    [SerializeField]
+    private RectTransform rightArrow;
+    private RectTransform RightArrow => rightArrow;
 
     private AbstractAnimation DialAnimation { get; set; }
     public bool IsAnimationPlaying { get; private set; }
@@ -33,16 +39,25 @@ public class GauntletUIController : MonoBehaviour
 
         DialAnimation?.Stop();
         IsAnimationPlaying = true;
-        DialAnimation = new Tween(1)
-            .For(Pivot)
-            .RotateLocalZTo(isNext ? 90 : -90)
-            .SetEasing(Easing.EaseInOutSine)
+        DialAnimation = new Flow()
+            .Queue(new Tween(1)
+                .For(Pivot)
+                .RotateLocalZTo(isNext ? 90 : -90)
+                .SetEasing(Easing.EaseInOutSine))
+            .At(0, new Tween(0.5f)
+                .For(isNext ? RightArrow : LeftArrow)
+                .MoveAnchoredPositionXTo(isNext ? 162 : -113)
+                .SetEasing(Easing.EaseInSine))
+            .At(0.5f, new Tween(0.5f)
+                .For(isNext ? RightArrow : LeftArrow)
+                .MoveAnchoredPositionXTo(isNext ? 142 : -93)
+                .SetEasing(Easing.EaseOutSine))
             .OnCompleted(() =>
-            {
-                IsAnimationPlaying = false;
-                SymbolUIImage.sprite = symbol.SymbolSprite;
-                Pivot.rotation = Quaternion.Euler(0, 0, 0);
-            })
+                {
+                    IsAnimationPlaying = false;
+                    SymbolUIImage.sprite = symbol.SymbolSprite;
+                    Pivot.rotation = Quaternion.Euler(0, 0, 0);
+                })
             .Start();
     }
 }
