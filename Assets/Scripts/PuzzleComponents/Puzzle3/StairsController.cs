666193565aace7a7422501f4f53034f28b2a4388
Pepperized <bradstap@hotmail.co.uk>
2025-05-03 19:26:00 +0100

    puzzle 3 vfx work

--- Assets/Scripts/PuzzleComponents/Puzzle3/StairsController.cs
+++ Assets/Scripts/PuzzleComponents/Puzzle3/StairsController.cs
@@ -1,3 +1,4 @@
+using FriedSynapse.FlowEnt;
 using UnityEngine;
 
 public class StairsController : MonoBehaviour
@@ -8,10 +9,37 @@ public class StairsController : MonoBehaviour
     [SerializeField]
     private GameObject hologramStairs;
     private GameObject HologramStairs => hologramStairs;
+    [SerializeField]
+    private Material stairsMat;
+    private Material StairsMat => stairsMat;
+    [SerializeField]
+    private float cutoffStart;
+    private float CutoffStart => cutoffStart;
+    [SerializeField]
+    private float cutoffEnd;
+    private float CutoffEnd => cutoffEnd;
+
+    private AbstractAnimation DestroyAnimation { get; set; }
+
+    private const string CutoffHeightString = "_Cuttoff_Height";
+
+    void Awake()
+    {
+        StairsMat.SetFloat(CutoffHeightString, CutoffStart);
+    }
 
     public void DestroyRealStairs()
     {
-        RealStairs.SetActive(false);
+        DestroyAnimation?.Stop();
+        DestroyAnimation = new Tween(4)
+            .For(StairsMat)
+            .FloatTo(CutoffHeightString, CutoffStart, CutoffEnd)
+            .OnCompleted(() =>
+            {
+                RealStairs.SetActive(false);
+            })
+            .Start();
+
     }
 
     public void SetHologramStarisEnabled(bool isEnabled)
