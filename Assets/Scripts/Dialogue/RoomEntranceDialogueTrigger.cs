b5d16fcf8903df0d2e588fd562ec44a01ceb458d
Jack Brewer <j.brewer@hotmail.co.uk>
2025-05-04 15:11:59 +0100

    Second room dialogue

---
+++ Assets/Scripts/Dialogue/RoomEntranceDialogueTrigger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.EventSystems.EventTrigger;
+
+public class RoomEntranceDialogueTrigger : MonoBehaviour
+{
+    private bool hasBeenTriggered;
+
+    [SerializeField]
+    private DialogueSectionSO entranceDialogue;
+    private DialogueSectionSO EntranceDialogue => entranceDialogue;
+
+    [SerializeField]
+    private UnityEvent onEntry;
+    private UnityEvent OnEntry => onEntry;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasBeenTriggered)
+        {
+            hasBeenTriggered = true;
+            OnEntry?.Invoke();
+            DialogueSingleton.Instance.OnSectionCompleted = null;
+            DialogueSingleton.Instance.EnqueueDialogue(EntranceDialogue);
+        }
+    }
+}
