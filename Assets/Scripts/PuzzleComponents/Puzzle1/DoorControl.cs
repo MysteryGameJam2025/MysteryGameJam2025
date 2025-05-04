103adf28414fbdf54b53ad9e24e631e556b853be
Jack Brewer <j.brewer@hotmail.co.uk>
2025-05-04 19:11:39 +0100

    Glitch when trying to leave

--- Assets/Scripts/PuzzleComponents/Puzzle1/DoorControl.cs
+++ Assets/Scripts/PuzzleComponents/Puzzle1/DoorControl.cs
@@ -49,8 +49,8 @@ public class DoorControl : SymbolActivatableBase
         }
         else
         {
-            LeftDoorPivot.rotation = Quaternion.identity;
-            RightDoorPivot.rotation = Quaternion.identity;
+            LeftDoorPivot.localRotation = Quaternion.identity;
+            RightDoorPivot.localRotation = Quaternion.identity;
         }
     }
 }
